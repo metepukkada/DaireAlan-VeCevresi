69c12033a28ae2c3b1f814bdc633015ae947475d
Metehan Öztürk <mete_pukkada@outlook.com>
2022-07-10 19:05:38 +0300

    Daire dilimi alanı formülü eklendi.

---
+++ src/DaireCevresiVeAlan.java
@@ -0,0 +1,23 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+public class DaireCevresiVeAlan {
+    public static void main(String[] args) {
+        double r, diliminAlanı, alan, cevre, alfa, pi = 3.1415;
+        Scanner girdi = new Scanner(System.in);
+        System.out.print("Dairenin Yarıçapını Giriniz: ");
+        r = girdi.nextDouble();
+
+        System.out.println("Dairenin Merkez Açısının Ölçüsünü Giriniz: ");
+        alfa = girdi.nextDouble();
+
+        diliminAlanı = (pi * (r*r) * alfa) / 360;
+        alan = r * r * pi;
+        cevre = 2 * pi * r;
+
+        System.out.println("Dairenin Yarıçapı: " + r );
+        System.out.println("Dairenin Çapı: " +(2 * r));
+        System.out.println("Dairenin Alanı: " + alan);
+        System.out.println("Dairenin Çevresi: "+ cevre);
+        System.out.println("Daire Diliminin Alanı: " + diliminAlanı);
+    }
+}
